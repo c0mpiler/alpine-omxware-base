a24bf1a394f5000f2bb16846089341ae485df154
Harsha Krishnareddy <hvkrishn@us.ibm.com>
2019-09-18 10:13:30 -0700

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 pip install --upgrade pip
 pip install --upgrade --no-cache-dir omxware
 
+# Should advice users to make use of the `add-host` docker coption instead.
 echo "9.1.34.33 omx-auth.sl.cloud9.ibm.com" >> /etc/hosts
 echo "9.1.35.108 omxware.sl.cloud9.ibm.com" >> /etc/hosts
 
